36af6ace90768bd2e198ceccf30061e9880b3d79
Hein <h.h.l.huijskes@student.utwente.nl>
2022-12-02 12:54:54 +0100

    Shorter because why not

--- src/2022/Day2-1.py
+++ src/2022/Day2-1.py
@@ -8,18 +8,9 @@ def getResult():
 
 
 def solve(data):
-    chunks = data.split('\n')
-    # Truncate the end, since it contains an empty string
-    chunks = chunks[:-1]
-    options = {'A X': 3+1, 'A Y': 6+2, 'A Z': 0+3,
-               'B X': 0+1, 'B Y': 3+2, 'B Z': 6+3,
-               'C X': 6+1, 'C Y': 0+2, 'C Z': 3+3}
-
-    score = 0
-    for chunk in chunks:
-        score += options[chunk]
-    solution = score
-    return solution
+    chunks = data.split('\n')[:-1]
+    opt = {'A X': 4, 'A Y': 8, 'A Z': 3, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 7, 'C Y': 2, 'C Z': 6}
+    return sum([(lambda c: opt[c])(c) for c in chunks])
 
 
 print(getResult())
