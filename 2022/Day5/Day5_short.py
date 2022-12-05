7d99fa930bc0772f26c8964a9f246e3cebd9ce86
Hein <h.h.l.huijskes@student.utwente.nl>
2022-12-05 12:07:14 +0100

    Day 5 improved

--- 2022/Day5/Day5_short.py
+++ 2022/Day5/Day5_short.py
@@ -1,41 +1,27 @@
-import re as s
+import re as r
 
 
 def getResult():
-    data = open('data.txt', 'r').read()
+    data = open('data.txt', 'r').read().split('\n')[:-1]
     print(solvePartOne(data))
     print(solvePartTwo(data))
 
 
 def solvePartOne(data):
-    stacks_list, instructions = data.split('\n\n')
-    stacks_list = stacks_list.split('\n 1')[0].split('\n')
-    instructions = instructions.split('\n')[:-1]
+    instructions, stackage = data[10:], data[:9]
 
-    stacks = [list() for i in range(0, (len(stacks_list[0])+5) // 4)]
-    for layer in stacks_list:
-        i = 0
-        stack = 1
-        for i in range(0, len(layer)):
-            char = layer[i]
-            if i % 4 == 0:
-                if char == '[':
-                    stacks[stack].append(layer[i+1])
-                stack += 1
-
-    for stack in stacks:
-        stack.reverse()
+    stacks = [[] for i in range(0, (len(stackage[0])+1)//4)]
+    for x in range(1, len(stackage[0]), 4):
+        for y in range(0, len(stackage)):
+            if stackage[y][x] != ' ':
+                stacks[(x-1)//4].insert(0, stackage[y][x])
 
     for instruction in instructions:
-        [x, y, z] = [int(x) for x in s.findall(r'\d+', instruction)]
+        [x, y, z] = [int(x) for x in r.findall(r'\d+', instruction)]
         for i in range(0, x):
-            stacks[z].append(stacks[y].pop())
+            stacks[z-1].append(stacks[y-1].pop())
 
-    result = ''
-    for stack in stacks:
-        if stack:
-            result += stack.pop()
-    return result
+    return ''.join([s.pop() for s in stacks])
 
 
 def solvePartTwo(data):
