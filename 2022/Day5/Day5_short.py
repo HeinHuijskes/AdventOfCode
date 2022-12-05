39df1598d26159e0e801ff6fe9c8ea9f4211c36e
Hein <h.h.l.huijskes@student.utwente.nl>
2022-12-05 12:19:50 +0100

    Day 5 further improved

--- 2022/Day5/Day5_short.py
+++ 2022/Day5/Day5_short.py
@@ -8,25 +8,26 @@ def getResult():
 
 
 def solvePartOne(data):
-    instructions, stackage = data[10:], data[:9]
-
-    stacks = [[] for i in range(0, (len(stackage[0])+1)//4)]
-    for x in range(1, len(stackage[0]), 4):
-        for y in range(0, len(stackage)):
-            if stackage[y][x] != ' ':
-                stacks[(x-1)//4].insert(0, stackage[y][x])
-
-    for instruction in instructions:
-        [x, y, z] = [int(x) for x in r.findall(r'\d+', instruction)]
-        for i in range(0, x):
-            stacks[z-1].append(stacks[y-1].pop())
-
-    return ''.join([s.pop() for s in stacks])
+    # Separate the instructions and the initial state of crates
+    ins, ini = data[10:], data[:9]
+    # Prepare an array to store the stacks of crates in
+    cra = [[] for i in range(0, (len(ini[0])+1)//4)]
+    # Parse the current state of crates and store in the stack
+    [(lambda: cra[(x-1)//4].insert(0, ini[y][x]) if ini[y][x] != ' ' else 0)() for y in range(0, len(ini)) for x in range(1, len(ini[0]), 4)]
+    # Parse the instructions and apply them to the stack
+    [[cra[c-1].append(cra[b-1].pop()) for i in range(0, a)] for [a, b, c] in [[int(x) for x in r.findall(r'\d+', i)] for i in ins]]
+    # Collect the top of each array in the stack
+    return ''.join([c.pop() for c in cra])
 
 
 def solvePartTwo(data):
-    # ADD SOLUTION BELOW
-    return data[0]
+    # Same as part 1
+    ins, ini = data[10:], data[:9]
+    cra = [[] for i in range(0, (len(ini[0])+1)//4)]
+    [(lambda: cra[(x-1)//4].insert(0, ini[y][x]) if ini[y][x] != ' ' else 0)() for y in range(0, len(ini)) for x in range(1, len(ini[0]), 4)]
+    # Same as part 1, but collect the boxes to a list first, and reverse it
+    [[cra[c-1].append(box) for box in list(reversed([cra[b-1].pop() for i in range(0, a)]))] for [a, b, c] in [[int(x) for x in r.findall(r'\d+', i)] for i in ins]]
+    return ''.join([c.pop() for c in cra])
 
 
 getResult()
