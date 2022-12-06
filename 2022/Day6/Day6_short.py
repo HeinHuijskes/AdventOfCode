6327fdd5540f0786de906a6b3a1bf8d41fae03bf
Hein <h.h.l.huijskes@student.utwente.nl>
2022-12-06 10:14:57 +0100

    Day 6 - One-liner

--- 2022/Day6/Day6_short.py
+++ 2022/Day6/Day6_short.py
@@ -5,13 +5,11 @@ def getResult():
 
 
 def solvePartOne(data):
-    # ADD SOLUTION BELOW
-    return data[0]
+    return [x for x in [i + 4 if (lambda c: (lambda f, *a: f(f, *a))(lambda rec, c: True if len(c) == 1 else (lambda fun, l: False if l[0] in l[1:] else fun(fun, l[1:]))(rec, c), c))(data[0][i:i + 4]) else 0 for i in range(0, len(data[0]))] if x != 0][0]
 
 
 def solvePartTwo(data):
-    # ADD SOLUTION BELOW
-    return data[0]
+    return [x for x in [i + 14 if (lambda c: (lambda f, *a: f(f, *a))(lambda rec, c: True if len(c) == 1 else (lambda fun, l: False if l[0] in l[1:] else fun(fun, l[1:]))(rec, c), c))(data[0][i:i + 14]) else 0 for i in range(0, len(data[0]))] if x != 0][0]
 
 
 getResult()
