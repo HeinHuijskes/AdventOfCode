f03cc77751b34bd412996826d3d7205876c9a19a
Hein <h.h.l.huijskes@student.utwente.nl>
2022-12-06 13:01:36 +0100

    Day 6 - Nicer day 6

--- 2022/Day6/Day6_better.py
+++ 2022/Day6/Day6_better.py
@@ -1,17 +1,29 @@
 def getResult():
-    data = open('data.txt', 'r').read().split('\n')[:-1]
+    data = open('data.txt', 'r').read().split('\n')[0]
     print(solvePartOne(data))
     print(solvePartTwo(data))
 
 
 def solvePartOne(data):
-    # ADD SOLUTION BELOW
-    return data[0]
+    for i in range(0, len(data)):
+        chars = data[i:i+4]
+        if chars[0] not in chars[1:] and chars[1] not in chars[2:] and chars[2] not in chars[3:]:
+            return i + 4
 
 
 def solvePartTwo(data):
-    # ADD SOLUTION BELOW
-    return data[0]
+    def recursion(chars):
+        if len(chars) == 1:
+            return True
+        elif chars[0] in chars[1:]:
+            return False
+        else:
+            return recursion(chars[1:])
+
+    n = 14
+    for i in range(0, len(data)):
+        if recursion(data[i:i+n]):
+            return i + n
 
 
 getResult()
