4712daee232fd29b0d7943c625036f8bfa0d8167
Hein <h.h.l.huijskes@student.utwente.nl>
2022-12-07 17:59:00 +0100

    Day 7

---
+++ 2022/Day7/Day7_better.py
@@ -0,0 +1,17 @@
+def getResult():
+    data = open('data.txt', 'r').read().split('\n')[:-1]
+    print(solvePartOne(data))
+    print(solvePartTwo(data))
+
+
+def solvePartOne(data):
+    # ADD SOLUTION BELOW
+    return data[0]
+
+
+def solvePartTwo(data):
+    # ADD SOLUTION BELOW
+    return data[0]
+
+
+getResult()
