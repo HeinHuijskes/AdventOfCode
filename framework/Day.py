2219719b0273198dc6b5e9e8203620ae5bf7fcfd
Hein <h.h.l.huijskes@student.utwente.nl>
2022-12-10 20:54:28 +0100

    Add inheritance to reduce the number of lines per day

---
+++ framework/Day.py
@@ -0,0 +1,17 @@
+class Day:
+    def getResult(self):
+        test_data = open('testdata.txt', 'r').read()
+        if len(test_data) > 0:
+            test_data = test_data.split('\n')[:-1]
+            print('Test answer part 1:', self.solvePartOne(test_data))
+            print('Test answer part 2:', self.solvePartTwo(test_data))
+
+        data = open('data.txt', 'r').read().split('\n')[:-1]
+        print('Answer part 1:', self.solvePartOne(data))
+        print('Answer part 2:', self.solvePartTwo(data))
+
+    def solvePartOne(self, data):
+        return 'No part 1 solution yet'
+
+    def solvePartTwo(self, data):
+        return 'No part 2 solution yet'
