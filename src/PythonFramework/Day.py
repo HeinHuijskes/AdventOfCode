6487025f40c6b9c1c378e497d6031c77c28da725
Hein <h.h.l.huijskes@student.utwente.nl>
2023-12-06 09:04:09 +0100

    Small addition to framework

--- src/PythonFramework/Day.py
+++ src/PythonFramework/Day.py
@@ -1,11 +1,13 @@
 class Day:
-    def getResult(self):
+    def getResult(self, testOnly = False):
         test_data = open('testdata.txt', 'r').read()
         if len(test_data) > 0:
             test_data = test_data.split('\n')[:-1]
             print('Test answer part 1:', self.solvePartOne(test_data))
             print('Test answer part 2:', self.solvePartTwo(test_data))
 
+        if testOnly:
+            return
         data = open('data.txt', 'r').read().split('\n')[:-1]
         print('Answer part 1:', self.solvePartOne(data))
         print('Answer part 2:', self.solvePartTwo(data))
