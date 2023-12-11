eda4fcaa4d49bdb32458d0974e4bcae9cb4fd4ca
Hein <h.h.l.huijskes@student.utwente.nl>
2023-12-11 17:15:52 +0100

    Add timer because why not

--- src/PythonFramework/Day.py
+++ src/PythonFramework/Day.py
@@ -1,16 +1,32 @@
+import time
+
 class Day:
+    start_time = 0
+    inter_time = 0
+    def updateTime(self):
+        self.inter_time = time.perf_counter() - self.start_time - self.inter_time
+
+    def displayAnswer(self, part, answer, test=False, timeFactor = 10000):
+        self.updateTime()
+        if test:
+            print(f'Test {part}   ({int(self.inter_time*timeFactor)/timeFactor} sec): {answer}')
+        else: 
+            print(f'Answer {part} ({int(self.inter_time*timeFactor)/timeFactor} sec): {answer}')
+
     def getResult(self, testOnly = False):
+        self.start_time = time.perf_counter()
         test_data = open('testdata.txt', 'r').read()
         if len(test_data) > 0:
             test_data = test_data.split('\n')[:-1]
-            print('Test answer part 1:', self.solvePartOne(test_data))
-            print('Test answer part 2:', self.solvePartTwo(test_data))
+            self.displayAnswer(1, self.solvePartOne(test_data), test=True)
+            self.displayAnswer(2, self.solvePartTwo(test_data), test=True)
 
         if testOnly:
             return
         data = open('data.txt', 'r').read().split('\n')[:-1]
-        print('Answer part 1:', self.solvePartOne(data))
-        print('Answer part 2:', self.solvePartTwo(data))
+        self.displayAnswer(1, self.solvePartOne(data))
+        self.displayAnswer(2, self.solvePartTwo(data))
+        print(f'Total time: {time.perf_counter() - self.start_time} sec')
 
     def solvePartOne(self, data):
         return 'No part 1 solution yet'
