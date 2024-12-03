dfdc2503ec0b30ef044bd91d25d5bdf77023f167
Hein <h.h.l.huijskes@student.utwente.nl>
2024-12-03 10:24:23 +0100

    Improve code

--- src/year2024/Day3/Day3_normal.py
+++ src/year2024/Day3/Day3_normal.py
@@ -11,30 +11,17 @@ class Day3(Day):
         line = ''.join(data)
         mults = re.findall("mul\(\d{1,3},\d{1,3}\)", line)
         numbers = [[int(y) for y in x[4:-1].split(',')] for x in mults]
-        result = 0
-        for number in numbers:
-            result += number[0] * number[1]
-        return result
+        return sum([nr[0]*nr[1] for nr in numbers])
 
     def solvePartTwo(self, data):
         numbers = []
         do = True
-        line = ''.join(data)
-        mults = re.findall("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", line)
-        numbers = []
-        for mult in mults:
-            if mult[:2] == "do":
-                if mult == "do()":
-                    do = True
-                elif mult == "don't()":
-                    do = False
-                continue
-            if do:
-                numbers += [[int(y) for y in mult[4:-1].split(',')]]
-        result = 0
-        for number in numbers:
-            result += number[0] * number[1]
-        return result
+        for instruction in re.findall("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", ''.join(data)):
+            if instruction[:2] == "do":
+                do = instruction == "do()"
+            elif do:
+                numbers += [[int(y) for y in instruction[4:-1].split(',')]]
+        return sum([nr[0]*nr[1] for nr in numbers])
 
 
 Day3(3).getResult()
