b8010f6d5ef095fa32f5a8b3f64e55c112a7a28f
Hein <h.h.l.huijskes@student.utwente.nl>
2024-12-12 15:08:21 +0100

    Day 12 2024 (faster)

--- src/year2024/Day12/Day12.py
+++ src/year2024/Day12/Day12.py
@@ -6,15 +6,17 @@ from PythonFramework.Algorithms import *
 
 
 class Day12(Day):
+    cardinal_directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    def pad(self, grid, value):
+        new_grid = [[value for i in range(len(grid[0])+2)] for j in range(len(grid)+2)]
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                new_grid[y+1][x+1] = grid[y][x]
+        return new_grid
+
     def parse(self, data):
-        regions = {}
-        for y, line in enumerate(data):
-            for x, char in enumerate(line):
-                if char not in regions:
-                    regions[char] = []
-                regions[char].append((x, y))
-        return regions, data
-    
+        return self.pad([[char for char in line] for line in data], 0)
+
     def isNeighbour(self, x, y, x2, y2):
         return ((x == x2+1 or x == x2-1) and y == y2) or (x == x2 and (y == y2-1 or y == y2+1))
     
@@ -24,43 +26,45 @@ class Day12(Day):
             if (x2, y2) not in visited and self.isNeighbour(x, y, x2, y2):
                 neighbours.append((x2, y2))
         return neighbours
-
-    def defineRegions(self, regions):
-        result = []
-        visited = []
-        queue = []
-        for x, y in regions:
-            if (x, y) in visited:
-                continue
-            visited.append((x, y))
-            queue.append((x, y))
-            region = [(x, y)]
-            while len(queue) > 0:
-                x, y = queue.pop()
-                neighbours = self.getNeighbours(x, y, visited, regions)
-                queue += neighbours
-                visited += neighbours
-                region += neighbours
-            result.append(region)
-
-        return result
     
-    def findPerimeter(self, region):
+    def getNeighbours(self, x, y, visited, grid):
+        neighbours = []
+        value = grid[y][x]
+        for x_add, y_add in self.cardinal_directions:
+            x_val, y_val = x+x_add, y+y_add
+            if not visited[y_val][x_val] and grid[y_val][x_val] == value:
+                neighbours.append((x_val, y_val))
+                visited[y_val][x_val] = True
+        return neighbours
+
+    def getRegion(self, x, y, grid, visited):
+        result = [(x, y)]
+        visited[y][x] = True
+        i = 0
+        while i < len(result):
+            x, y = result[i]
+            result += self.getNeighbours(x, y, visited, grid)
+            i += 1
+        return result
+
+    def getPerimeterLength(self, region, grid):
         result = 0
         for x, y in region:
-            result += 4 - len(self.getNeighbours(x, y, [], region))
+            value = grid[y][x]
+            result += 4
+            for direction in self.cardinal_directions:
+                if grid[y+direction[1]][x+direction[0]] == value:
+                    result -= 1
         return result
     
     def findCorners(self, region, max_x, max_y):
-        # Pad with +2 to add a layer around the outside
-        grid = [[False for j in range(max_x+2)] for i in range(max_y+2)]
+        grid = [[False for j in range(max_x)] for i in range(max_y)]
         for x, y in region:
-            # Pad x and y too
-            grid[y+1][x+1] = True
-        corners = 0
+            grid[y][x] = True
 
-        for x in range(max_x+1):
-            for y in range(max_y+1):
+        corners = 0
+        for x in range(max_x-1):
+            for y in range(max_y-1):
                 # TL, TR, BL, BR
                 kernel = [grid[y][x],   grid[y][x+1], 
                           grid[y+1][x], grid[y+1][x+1]]
@@ -75,31 +79,30 @@ class Day12(Day):
 
     def solvePartOne(self, data):
         result = 0
-        data = data[0]
-        for region in data:
-            regions = data[region]
-            regions = self.defineRegions(regions)
-            for r in regions:
-                area = len(r)
-                perimeter = self.findPerimeter(r)
-                print(region, area, perimeter)
+        visited = [[val == 0 for val in line] for line in data]
+        for y, line in enumerate(data):
+            for x, value in enumerate(line):
+                if visited[y][x]:
+                    continue
+                region = self.getRegion(x, y, data, visited)
+                area = len(region)
+                perimeter = self.getPerimeterLength(region, data)
                 result += area * perimeter
-
         return result
 
     def solvePartTwo(self, data):
         result = 0
-        data, grid = data
-        max_x = len(grid[0])
-        max_y = len(grid)
-        for region in data:
-            regions = data[region]
-            regions = self.defineRegions(regions)
-            for r in regions:
-                area = len(r)
-                sides = self.findCorners(r, max_x, max_y)
-                result += area * sides
-
+        max_x = len(data[0])
+        max_y = len(data)
+        visited = [[val == 0 for val in line] for line in data]
+        for y, line in enumerate(data):
+            for x, value in enumerate(line):
+                if visited[y][x]:
+                    continue
+                region = self.getRegion(x, y, data, visited)
+                area = len(region)
+                perimeter = self.findCorners(region, max_x, max_y)
+                result += area * perimeter
         return result
 
 
