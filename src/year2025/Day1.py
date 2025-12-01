05b3e875a57a49370907c2fa888aaee835ae58cd
Hein <h.h.l.huijskes@student.utwente.nl>
2025-12-01 13:42:16 +0100

    Day 1 2025

--- src/year2025/Day1.py
+++ src/year2025/Day1.py
@@ -3,14 +3,32 @@ import src.PythonFramework.Algorithms as algs
 
 
 class Solver(Day):
-    test_answers = [None, None]
-    answers = [None, None]
+    test_answers = [3, 6]
+    answers = [1034, 6166]
 
     def parse(self, data: list[str]):
-        return data
+        self.dirs = {'R': 1, 'L': -1}
+        return [(entry[0], int(entry[1:])) for entry in data]
 
     def solvePartOne(self, data):
-        return 'No part 1 solution yet'
+        state, counter = 50, 0
+        for dir, amount in data:
+            state = (state + self.dirs[dir] * amount) % 100
+            counter += state == 0
+        return counter
 
     def solvePartTwo(self, data):
-        return 'No part 2 solution yet'
+        state, counter = 50, 0
+
+        for dir, amount in data:
+            hundreds = amount // 100
+            amount -= hundreds * 100
+
+            newstate = (state + self.dirs[dir] * amount)
+            newerstate = newstate % 100
+            pass_zero = (newerstate == 0) or (newerstate != newstate and state != 0)
+
+            counter += pass_zero + hundreds
+            state = newerstate
+
+        return counter
