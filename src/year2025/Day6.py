d1bd322b32135aac603541f146ab63311d0b93af
Hein <h.h.l.huijskes@student.utwente.nl>
2025-12-06 09:59:52 +0100

    Small rewrite

--- src/year2025/Day6.py
+++ src/year2025/Day6.py
@@ -7,40 +7,25 @@ from math import prod
 class Solver(Day):
     test_answers = [4277556, 3263827]
     answers = [6635273135233, 12542543681221]
-
-    def parse(self, data: list[str]):
-        return data
+    operation = {'*': prod, '+': sum}
 
     def solvePartOne(self, data):
         data = [re.sub('[\ ]+', ' ', string).split() for string in data]
         data = [[int(x) if x not in ['*', '+'] else x for x in line] for line in data]
         data = [[data[j][i] for j in range(len(data))] for i in range(len(data[0]))]
-        result = 0
-        for col in data:
-            numbers = col[:-1]
-            operator = col[-1]
-            if operator == '+':
-                result += sum(numbers)
-            else:
-                result += prod(numbers)
-        return result
+        return sum([self.operation[col[-1]](col[:-1]) for col in data])
 
     def solvePartTwo(self, data):
+        # Transpose data to look at columns
         data = [[data[j][i] for j in range(len(data))] for i in reversed(range(len(data[0])))]
         data = [''.join(l).replace(' ', '') for l in data]
-        result = 0
-        numbers = []
+        # Separate operators
+        data = [re.sub('[+*]','',data[i]) if data[i] != '' else data[i-1][-1] for i in range(len(data))] + [data[-1][-1]]
+        result, numbers = 0, []
         for string in data:
-            operator = ''
-            if string == '':
+            if string in ['+', '*']:
+                result += self.operation[string](numbers)
+                numbers = []
                 continue
-            if string[-1] in ['+', '*']:
-                string, operator = string[:-1], string[-1]
             numbers.append(int(string))
-            if operator != '':
-                if operator == '+':
-                    result += sum(numbers)
-                else:
-                    result += prod(numbers)
-                operator, numbers = '', []
         return result
