f4875659bbb89b1661bd93b214f8576d8fbb10ed
Hein <h.h.l.huijskes@student.utwente.nl>
2025-12-11 21:46:28 +0100

    Day 11 improved

--- src/year2025/Day11.py
+++ src/year2025/Day11.py
@@ -1,10 +1,9 @@
 from src.PythonFramework.Day import Day
-import src.PythonFramework.Algorithms as algs
 
 
 class Solver(Day):
     test_answers = [5, 2]
-    answers = [472, None]
+    answers = [472, 526811953334940]
 
     def parse(self, data: list[str]):
         connections = {}
@@ -25,82 +24,55 @@ class Solver(Day):
                 for node in nodes:
                     queue.append(path+[node])
         return len(results)
+    
+    def walkThroughTree(self, data, start, goal):
+        vars = {start: []}
+        values = {start: 1}
+        queue = [start]
+
+        # Build a reversed tree in from the starting point outwards
+        while len(queue) > 0:
+            node = queue.pop(0)
+            # Skip end nodes
+            if node == 'out':
+                continue
+
+            children = data[node]
+            for child in children:
+                if child not in vars:
+                    vars[child] = []
+                    # Initially set each node to 0
+                    values[child] = 0
+                # Add parent node only once
+                if node not in vars[child]:
+                    vars[child].append(node)
+                # Only consider each node once
+                if child not in queue:
+                    queue.append(child)
+
+        # Now one by one collapse all variables from start to goal
+        while len(vars) > 0:
+            # Get currently collapsed variables
+            collapsed = [node for node in vars if len(vars[node]) == 0]
+            if goal in collapsed:
+                return values[goal]
+
+            for node in collapsed:
+                # Get collapsed value
+                vars.pop(node)
+                value = values[node]
+                for var in vars:
+                    parents = vars[var]
+                    # Check that this variable didn't sneakily collapse while running the loop
+                    # and consider only variables that have the current node as a parent
+                    if len(parents) > 0 and node in parents:
+                        # Remove the collapsed node from the parents
+                        parents.pop(parents.index(node))
+                        # And add the parents' value to the current node
+                        values[var] += value
 
     def solvePartTwo(self, data):
-        r_data = {}
-        for key in data:
-            values = data[key]
-            for value in values:
-                if value not in r_data:
-                    r_data[value] = []
-                r_data[value].append(key)
-
-        goal = 'svr'
-        queue = [['fft']]
-        fft_results = []
-        while len(queue) > 0:
-            path = queue.pop()
-            if path[-1] == goal:
-                fft_results.append(path)
-            else:
-                nodes = r_data[path[-1]]
-                for node in nodes:
-                    queue.append(path+[node])
-
-        lengths = {}
-        for result in fft_results:
-            if len(result) not in lengths:
-                lengths[len(result)] = 0
-            lengths[len(result)] += 1
-
-        goal = 'out'
-        queue = [['dac']]
-        dac_results = []
-        while len(queue) > 0:
-            path = queue.pop()
-            if path[-1] == goal:
-                dac_results.append(path)
-            else:
-                nodes = data[path[-1]]
-                for node in nodes:
-                    queue.append(path+[node])
-
-        lengths = {}
-        for result in dac_results:
-            if len(result) not in lengths:
-                lengths[len(result)] = 0
-            lengths[len(result)] += 1
-
-        dacs_blacklist = []
-        for result in dac_results:
-            for node in result:
-                if node != 'dac' and node not in dacs_blacklist:
-                    dacs_blacklist.append(node)
-
-        m_data = {}
-        for key in data:
-            value = data[key]
-            if key not in dacs_blacklist:
-                m_data[key] = value
-            else:
-                m_data[key] = []
-
-        goal = 'dac'
-        queue = [['fft']]
-        results = []
-        while len(queue) > 0:
-            path = queue.pop()
-            if path[-1] == goal:
-                results.append(path)
-            else:
-                nodes = m_data[path[-1]]
-                for node in nodes:
-                    queue.append(path+[node])
-
-        lengths = {}
-        for result in results:
-            if len(result) not in lengths:
-                lengths[len(result)] = 0
-            lengths[len(result)] += 1
-
-        return len(fft_results) * len(dac_results) * len(results)
+        fft = self.walkThroughTree(data, start='svr', goal='fft')
+        dac = self.walkThroughTree(data, start='fft', goal='dac')
+        out = self.walkThroughTree(data, start='dac', goal='out')
+        return fft * dac * out
